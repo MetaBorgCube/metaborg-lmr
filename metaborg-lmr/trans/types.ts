175cf7fb8b6a00cea45f7d95a04e985ad5f534fa
Eelco Visser <e.visser@tudelft.nl>
2015-10-26 16:09:16 -0400

    split syntax into modules and removed unnecessary constructors

--- metaborg-lmr/trans/types.ts
+++ metaborg-lmr/trans/types.ts
@@ -5,19 +5,24 @@ imports src-gen/signatures/LMR-sig
 type rules
 
   TXInt() : TInt()
+  
   TXBool() : TBool()
+  
   TXArrow(t1, t2) : TArrow(ts1, ts2)
     where t1 : ts1 and t2 : ts2
+    
   TXRec(e) : t
     where e : t
 
-  ModUnqualRef(ModRefId(name)) : t
+type rules
+
+  ModRef(name) : t
     where definition of name : t
     
-  ModQualRef(ref, ModRefId(name)) : t
+  ModQRef(ref, name) : t
     where definition of name : t
       
-  VarUnqualRef(name) : t
+  VarRef(name) : t
     where definition of name : t
     
   FldAccess(ref, name) : t
@@ -32,10 +37,10 @@ type rules
       and t1 == t2
           else error "type mismatch" on e
     
-  TypeUnqualRef(name) : t
+  TypeRef(name) : t
     where definition of name : t
     
-  TypeQualRef(e, name) : t
+  TypeQRef(e, name) : t
     where definition of name : t
     
   Fun(arg, e) : TArrow(t1, t2)
@@ -47,9 +52,9 @@ type rules
   False() : TBool()
   True() : TBool()
   
-  Add(e1, e2) : TInt()
+  Add(e1, e2) + Mul(e1, e2) : TInt()
     where e1 : TInt()
-      and e2 : TInt()    
+      and e2 : TInt() 
   
   App(e1, e2) : t2
     where e1 : TArrow(t1, t2)
